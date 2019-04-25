0728e6ae4c4711e0a17b9a58fd2b0ca5cb067187
Jock2018 <1377483978@qq.com>
2019-04-25 12:10:26 +0800

    modified insertion_sort.py

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -10,10 +10,9 @@ def insertion_sort(collection):
     # 插入排序
     length = len(collection)
     for i in range(1, length):    # 第一个数作为初始有序序列，所以不用比较
-        tmp = collection[i]
         j = i - 1
-        while j >= 0 and collection[j] > tmp:
-            collection[j+1], collection[j] = collection[j], tmp
+        while j >= 0 and collection[j + 1] < collection[j]:
+            collection[j], collection[j + 1] = collection[j + 1], collection[j]
             j -= 1
     return collection
 
